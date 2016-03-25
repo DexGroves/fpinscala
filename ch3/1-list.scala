a4eee169d596ef3c21159c839e494f88ab56bf75
DexGroves <dex.groves@gmail.com>
2016-03-25 22:18:50 +0000

    Folding, as far as inefficient 3.12 solution.

--- ch3/1-list.scala
+++ ch3/1-list.scala
@@ -2,7 +2,6 @@ sealed trait List[+A]
 case object Nil extends List[Nothing]
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
-
 object List{
   def sum(ints: List[Int]): Int = ints match {
     case Nil => 0
@@ -18,6 +17,12 @@ object List{
   def apply[A](as: A*): List[A] =
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
+
+  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B =
+    as match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
 
   /* Exercise 3.2 */
   def tail[A](as: List[A]): List[A] = as match {
@@ -61,6 +66,48 @@ object List{
     case Cons(h, Nil) => Nil
     case Cons(h, t) => Cons(h, init(t))
   }
+
+  /*Exercise 3.9 */
+  def length[A](as: List[A]): Int = {
+    foldRight(as, 0)((x, y) => 1 + y)
+  }
+
+  /* Exercise 3.10 */
+  @annotation.tailrec
+  def foldLeft[A,B](as: List[A], z: B)(f: (B, A) => B): B = {
+    as match {
+      case Nil => z
+      case Cons(h, t) => foldLeft(t, f(z, h))(f)
+    }
+  }
+
+  /* Exercise 3.11 */
+  /* I can't get these to work for undefined types. Will come back later */
+  // def sumLeft[A](as: List[A]) = {
+  //   foldLeft(as, 0)((a1: A, a2: A) => a1.+(a2))
+  // }
+
+  // def productLeft[A](as: List[A]) = {
+  //   foldLeft(as, 1)(_ * _)
+  // }
+
+  def lengthLeft[A, B](as: List[A]): Int =
+    foldLeft(as, 0)((n: Int, a: A) => n + 1)
+
+  /* Exercise 3.12 */
+  def last[A](as: List[A]): A =
+    as match {
+      case Cons(h, Nil) => h
+      case Cons(h, t) => last(t)
+    }
+
+  def reverse[A](as: List[A]): List[A] = {
+    as match {
+      case Nil => Nil
+      case Cons(h, Nil) => Cons(h, Nil)
+      case Cons(h, t) => Cons(last(as), reverse(init(as)))
+    }
+  }
 }
 
 val seq5i = List(1,2,3,4,5)
@@ -70,3 +117,7 @@ val seq5i = List(1,2,3,4,5)
 // List.drop(seq5i, 3)
 // List.dropWhile(seq5i, (x: Int) => x % 2 == 0)
 // List.init(seq5i)
+// List.length(seq5i)
+// List.foldLeft(seq5i, 1)(_ * _)
+// List.lengthLeft(seq5i)
+List.reverse(seq5i)
