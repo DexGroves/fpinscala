e99167d24dbe99129ac4ed7f3e9db5e35ef267c0
DexGroves <dex.groves@gmail.com>
2016-04-16 18:38:29 +0100

    Finish chapter 3 exercises

--- ch3/3-tree.scala
+++ ch3/3-tree.scala
@@ -34,6 +34,31 @@ object Tree {
       case Branch(l, r) => Branch(map(l)(f), map(r)(f))
     }
   }
+
+  /* Exercise 3.29 */
+  def fold[A,B,C](tree: Tree[A])(fLeaf: A => B)(fBranch: (B, B) => B): B = {
+    tree match {
+      case Leaf(x) => fLeaf(x)
+      case Branch(l, r) => fBranch(fold(l)(fLeaf)(fBranch),
+                                   fold(r)(fLeaf)(fBranch))
+    }
+  }
+
+  def sizeViaFold[A](tree: Tree[A]): Int = {
+    fold(tree)(x => 1)((l, r) => l + r + 1)
+  }
+
+  def maximumViaFold(tree: Tree[Int]): Int = {
+    fold(tree)(x => x)((l, r) => l max r)
+  }
+
+  def depthViaFold[A](tree: Tree[A]): Int = {
+    fold(tree)(x => 0)((l, r) => (l max r) + 1)
+  }
+
+  def mapViaFold[A](tree: Tree[A])(f: A => A): Tree[A] = {
+    fold(tree)(x => Leaf(f(x)): Tree[A])((l, r) => Branch(l, r))
+  }
 }
 
 val testTree = Branch(Branch(Branch(Leaf(1), Leaf(2)), Leaf(3)), Leaf(4))
@@ -42,3 +67,7 @@ Tree.size(testTree)
 Tree.maximum(testTree)
 Tree.depth(testTree)
 Tree.map(testTree)(_ * 2)
+Tree.sizeViaFold(testTree)
+Tree.maximumViaFold(testTree)
+Tree.depthViaFold(testTree)
+Tree.mapViaFold(testTree)(_ * 2)
